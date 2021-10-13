5d4973a0ce520be4ab7f8a5b0c2b5b66f338f3e4
Alvin-laptop <chun.alvinmok@gmail.com>
2021-10-13 10:05:43 +0100

    load testing

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = MultiApp()
 # file_title and func are for system settings and should always be named in such way (e.g. file_title all small letters no space)
 # e.g. intro.py = "intro"     for file_title
 #                 "intro.app" for func
-app.add_app("About", "intro", intro.app)
 app.add_app("Sun Path and Shading Analysis", "sunpath", sunpath.app)
+app.add_app("About", "intro", intro.app)
 app.add_app("Solar Geometry Subplots", "SolarGeo_subplots_solartime", SolarGeo_subplots_solartime.app)
 app.add_app("Annual Solar Irradiation Surface Plot", "SolarIrradiation_Aniso", SolarIrradiation_Aniso.app)
 app.add_app("Weather Analysis", "WeatherAnalysis", WeatherAnalysis.app)
