b3b0412a209df7ff290923ae131ba74899ace871
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-01-15 16:18:44 +0000

    Increase test timeout

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,8 @@ python3 -m venv "$venv_dir"
 
 # Install dependencies
 $pip install --quiet --upgrade -r requirements.txt
+
+# Install app program files
 cp --recursive app.py multiapp.py apps .streamlit "app_dir/"
 
 # Install Streamlit service
--- test_app.py
+++ test_app.py
@@ -1,13 +1,16 @@
 # Streamlit tests
 # https://docs.streamlit.io/develop/api-reference/app-testing
 
-
+import os
 import streamlit.testing.v1
 
 
-def test_app():
-    app_test = streamlit.testing.v1.AppTest.from_file("app.py")
+def test_app(path: str = None, timeout: int = None):
+    path = path or os.getenv("STREAMLIT_APP_PATH", "app.py")
+    timeout = int(timeout or os.getenv('STREAMLIT_TEST_TIMEOUT', 60))
 
-    app_test.run(timeout=10)
+    app_test = streamlit.testing.v1.AppTest.from_file(path)
+
+    app_test.run(timeout=timeout)
 
     assert not app_test.exception
